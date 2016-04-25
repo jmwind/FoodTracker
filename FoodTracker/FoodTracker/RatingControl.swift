6f6576346433db171ff1566f1f80f1a558fa2d3d
Jean-Michel Lemieux <jmwind@gmail.com>
2016-04-25 17:07:44 -0400

    New

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,25 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Jean-Michel Lemieux on 2016-04-25.
+//  Copyright © 2016 Jean-Michel Lemieux. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
