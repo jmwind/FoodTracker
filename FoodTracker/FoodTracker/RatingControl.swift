c81a0942848f2a3273cce700e177d7f51e623633
Jean-Michel Lemieux <jmwind@gmail.com>
2016-04-25 17:50:48 -0400

    bouncing stuff with stars

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -10,8 +10,59 @@ import UIKit
 
 class RatingControl: UIView {
 
+    var rating = 0
+    var ratingButtons = [UIButton]()
+    
+    let spacing = 5
+    let starCount = 5
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        for _ in 0..<starCount {
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            button.backgroundColor = UIColor.redColor()
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+    }
+    
+    override func layoutSubviews() {
+        // Set the button's width and height to a square the size of the frame's height.
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        // Offset each button's origin by the length of the button plus some spacing.
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
+            button.frame = buttonFrame
+        }
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        return CGSize(width: width, height: buttonSize)
+    }
+    
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+        
+        
+        UIView.animateWithDuration(2.0,
+            delay: 0,
+            usingSpringWithDamping: 0.2,
+            initialSpringVelocity: 2.0,
+            options: UIViewAnimationOptions.AllowUserInteraction,
+            animations: {
+                    button.transform = CGAffineTransformMakeScale(0.825, 0.825)
+            },
+            //completion: nil
+            completion: { finish in
+                    button.transform = CGAffineTransformIdentity
+                
+            }
+        )
     }
     
     /*
@@ -21,5 +72,5 @@ class RatingControl: UIView {
         // Drawing code
     }
     */
-
 }
+
